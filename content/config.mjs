9a35e67f4a2c6c87ad5e191a60bbe3375b5f656a
NardoDragon <142852876+NardoDragon@users.noreply.github.com>
2023-09-16 07:35:29 -0500

    Update config.mjs

--- content/config.mjs
+++ content/config.mjs
@@ -1,1 +1,10 @@
-export default {};
+const config = {
+  title: "NardoDragon XBC3",
+  description: "Nardo's Adventure through XBC3",
+  author: "Nardo",
+  domain: "https://nardo-notes.vercel.app/",
+  // links to the pages you want to link to in the navbar and in the footer
+  navLinks: [{ href: "/Characters", name: "Characters" },{ href: "/Concepts", name: "Concepts" },{ href: "/Gameplay", name: "Gameplay" },{ href: "/Places", name: "Places" },{ href: "/_all", name: "All" }],
+};
+
+export default config;
